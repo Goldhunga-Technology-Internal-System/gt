3909ff91aabff5080b3958f27ac94e156f69f224
aryan-brahmabytelab <aryanbrahmabytelab@gmail.com>
2026-07-28 14:34:04 +0545

    refactor : refactor the auth user model

--- src/gt/auth/models/auth_user_model.py
+++ src/gt/auth/models/auth_user_model.py
@@ -1,5 +1,7 @@
 import uuid
+from datetime import datetime
 
+from sqlalchemy import Boolean, DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -21,4 +23,16 @@ class AuthUserModel:
     )
     email: Mapped[str] = mapped_column(
         unique=True, nullable=False, index=True, init=False
+    )
+    avatar_bg: Mapped[str] = mapped_column(String(255), nullable=False, init=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True, init=False)
+    is_onboarded: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, index=True, init=False, default=False
+    )
+    status: Mapped[str] = mapped_column(
+        String(255), nullable=False, default="active", index=True, init=False
+    )
+    full_name: Mapped[str] = mapped_column(String(255), nullable=False, init=False)
+    email_verified_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), nullable=True, default=None
     )
